e41f79d954e4b2c9a7ec1fe51e86c7cafb2a6edd
kyle-main <kyledmain@gmail.com>
2023-01-29 21:13:32 -0500

    add atom theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {},
+    colors: {
+      atom: {
+        100: 'f1f2f4',
+        200: 'd4d7de',
+        300: 'b7bcc8',
+        400: '9aa2b1',
+        500: '7d879b',
+        600: '596273',
+        700: '373d48',
+        800: '282c34',
+        900: '16181d'
+      }
+    }
   },
   plugins: [],
 }
