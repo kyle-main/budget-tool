241e605699a57e2bf77b43b38c039bb5c4184444
kyle-main <kyledmain@gmail.com>
2023-01-29 21:23:49 -0500

    update theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,18 +3,19 @@ module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   darkMode: 'class',
   theme: {
-    extend: {},
-    colors: {
-      atom: {
-        100: 'f1f2f4',
-        200: 'd4d7de',
-        300: 'b7bcc8',
-        400: '9aa2b1',
-        500: '7d879b',
-        600: '596273',
-        700: '373d48',
-        800: '282c34',
-        900: '16181d'
+    extend: {
+      colors: {
+        atom: {
+          100: '#f1f2f4',
+          200: '#d4d7de',
+          300: '#b7bcc8',
+          400: '#9aa2b1',
+          500: '#7d879b',
+          600: '#4e5565',
+          700: '#373d48',
+          800: '#282c34',
+          900: '#16181d'
+        }
       }
     }
   },
