495d670c31d0284e8015ba9969da01de446809e9
kyle-main <kyledmain@gmail.com>
2023-02-08 12:30:42 -0500

    temp

--- server/sheets/NetworthSheet.py
+++ server/sheets/NetworthSheet.py
@@ -20,5 +20,60 @@ class NetworthSheet(Sheet):
         """ Get all the values from our spread sheet in a range """
         result = self.sheet.values().get(spreadsheetId=NETWORTH_SPREADSHEET_ID,
                                          range=NETWORTH_SHEET_RANGE).execute()
-        values = result.get('values', [])
-        return values
+        res = []
+        values = self.unicode_normalize_2d_list(result.get('values', []))
+        for i, rec in enumerate(values):
+            x = {}
+            if len(rec) >= 23:
+                # TODO: impl networth record data class to handle this functionality more cleanly
+                x['date'] = rec[0]
+                x['net_worth'] = rec[1]
+                x['net_worth_delta'] = rec[2]
+                x['assets'] = rec[4]
+                x['assets_delta'] = rec[5]
+                x['checking'] = rec[6]
+                x['savings'] = rec[7]
+                x['hsa'] = rec[8]
+                x['401k_w_match'] = rec[9]
+                x['401k_vested_difference'] = rec[10]
+                x['401k_vested'] = rec[11]
+                x['ira'] = rec[12]
+                x['stocks'] = rec[13]
+                x['debt'] = rec[15]
+                x['debt_delta'] = rec[16]
+                x['apple_credit'] = rec[17]
+                x['chase_credit'] = rec[18]
+                x['citi_credit'] = rec[19]
+                x['secu_credit'] = rec[20]
+                x['student_loans'] = rec[21]
+                x['car_loan'] = rec[22]
+                x = {k: self.financial_string_to_decimal(
+                    v) for (k, v) in x.items()}
+                res.append(x)
+        return res
+
+    def financial_string_to_decimal(self, s: str):
+        import re
+
+        # If the string contains letters leave it alone
+        if any(char.isalpha() for char in s):
+            return s
+
+        # Check if the string starts with a parenthesis
+        negative = False
+        if s[0] == "(":
+            negative = True
+            s = s[1:-1]
+
+        # Remove the comma separators
+        s = re.sub(",", "", s)
+
+        # Convert the string to a decimal number
+        number = float(s)
+
+        # Make the number negative if needed
+        if negative:
+            number = -number
+
+        # Return the number with 2 decimal places
+        return str(round(number, 2))
